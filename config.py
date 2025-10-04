4247cc0f15aab5bab916c6cac47595a7cb6c8760
KshitijD21 <kshitijdumbre21@gmail.com>
2025-10-04 12:08:16 -0700

    Refactor config.py to load environment variables and set OpenMP configurations

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-API_RECEIVER_URL = "http://localhost:5000/receive"
+import os
+from dotenv import load_dotenv
 
+# Load environment variables
+load_dotenv()
+
+# Set OpenMP environment variables if specified in .env
+if os.getenv('OMP_NUM_THREADS'):
+    os.environ['OMP_NUM_THREADS'] = os.getenv('OMP_NUM_THREADS')
+if os.getenv('KMP_DUPLICATE_LIB_OK'):
+    os.environ['KMP_DUPLICATE_LIB_OK'] = os.getenv('KMP_DUPLICATE_LIB_OK')
+
+# Configuration
+FLASK_PORT = int(os.getenv('FLASK_PORT', 9000))
+FLASK_HOST = os.getenv('FLASK_HOST', '127.0.0.1')
+API_RECEIVER_URL = f"http://{FLASK_HOST}:{FLASK_PORT}/receive"
